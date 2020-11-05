4f0b4c91f2cce8e22e8861f3895ec35df41f81fe
CodeMaker-Zhao <zlq1440169549@qq.com>
2020-11-05 23:16:31 +0800

    画图更改

--- DrawPic/pic1.py
+++ DrawPic/pic1.py
@@ -1,6 +1,8 @@
 import csv
 import numpy as np
+import sys
 from matplotlib import pyplot as plt
+from matplotlib.pyplot import MultipleLocator
 
 def readCsv(samplename,resultname,sampleindex):
     with open(samplename) as sn:
@@ -19,42 +21,47 @@ def readCsv(samplename,resultname,sampleindex):
     for i in range(0,tcount):
         currentIndex = totalEnergy.index(currentMax)
         del xlabel[currentIndex]
-        totalEnergy.remove(currentMax)            
+        totalEnergy.remove(currentMax) 
     return xlabel,totalEnergy
     
 def fit(xlabel,ylabel,n):
     poly = np.polyfit(xlabel,ylabel,deg=n)
     z = np.polyval(poly,xlabel)
     return z
-def roration(ax):
-    for ticklabel in ax.xaixs.get_ticklabels():
-        ticklabel.set_rotation(30)
+def drawPic(x,y,lw=5,lineCol='tab:orange',baseLineCol='r'):
+    fig,ax = plt.subplots(1,4,sharey=True)
+
+    sxlabel=['ratio','d0','n','d1']
+    ax[0].set_ylabel('total energy(Kwh/m³)', fontsize=20)
+    plt.setp(ax[0].get_yticklabels(),fontsize=15)
+
+    for a,xl,i in zip(ax,sxlabel,range(4)):
+        a.plot(x[i],y[i],linewidth=lw,color=lineCol)
+        a.spines['bottom'].set_linewidth(4)
+        a.axhline(y=23.61, c=baseLineCol, ls='--', lw=2)
+        a.grid(color='black',linestyle='-',linewidth=1)
+        a.spines['left'].set_linewidth(4)
+        a.spines['right'].set_linewidth(4)
+        a.spines['top'].set_linewidth(4)
+        a.set_xlabel(xl, fontsize=20)
+        plt.setp(a.get_xticklabels(),fontsize=15)
+
+    fig.subplots_adjust(wspace=0.25)
+    ax[0].set(ylim=(23.0,25.5))
+    plt.show()
+
 
 ratio,ratioTe = readCsv('sample1.csv','result1.csv',1)
 d0,d0Te = readCsv('sample2.csv','result2.csv',2)
 n,nTe = readCsv('sample3.csv','result3.csv',0)
 d1,d1Te = readCsv('sample4.csv','result4.csv',3)
 
-fig,ax = plt.subplots(1,4,sharey=True)
-
 ratioZ = fit(ratio,ratioTe,1)
 d0Z = fit(d0,d0Te,1)
 nZ = fit(n,nTe,3)
 d1Z =fit(d1,d1Te,1)
 
-ax[0].plot(ratio,ratioZ)
-ax[0].set_title('TotaleEnergy change with ratio')
-ax[1].plot(d0,d0Z)
-ax[1].spines['left'].set_color('white')
-ax[1].yaxis.set_ticks_position('right')
-ax[1].set_title('TotaleEnergy change with d0')
-ax[2].plot(n,nZ)
-ax[2].spines['left'].set_color('white')
-ax[2].yaxis.set_ticks_position('right')
-ax[2].set_title('TotaleEnergy change with n')
-ax[3].plot(d1,d1Z)
-ax[3].spines['left'].set_color('white')
-ax[3].yaxis.set_ticks_position('right')
-ax[3].set_title('TotaleEnergy change with d1')
-fig.subplots_adjust(wspace=0)
-plt.show()
+x = [ratio,d0,n,d1]
+y = [ratioZ,d0Z,nZ,d1Z]
+
+drawPic(x,y)
